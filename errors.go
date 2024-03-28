ecef23b7a052c38a5790c116ec4cc1e1eb536372
Shawn Catanzarite <me@shawncatz.com>
2024-03-28 23:11:44 +0000

    v0.1.4: stacktraces fixed (maybe?)

--- errors.go
+++ errors.go
@@ -19,7 +19,7 @@ func Wrap(err error, message string) error {
 }
 
 func Wrapf(err error, format string, args ...interface{}) error {
-	return errors.WrapPrefix(err, fmt.Sprintf(format, args...), 2)
+	return errors.WrapPrefix(err, fmt.Sprintf(format, args...), 1)
 }
 
 func Cause(err error) error {
@@ -38,12 +38,17 @@ func Cause(err error) error {
 }
 
 func StackTrace(err error) []string {
+	if err == nil {
+		return nil
+	}
+
+	list := errors.Wrap(err, 1).StackFrames()
 	cause := Cause(err)
-	list := errors.Wrap(cause, 1).StackFrames()
 	stack := []string{cause.Error()}
 
 	for _, frame := range list {
-		stack = append(stack, fmt.Sprintf("%s.%s %s:%d (0x%x)", frame.Package, frame.Name, frame.File, frame.LineNumber, frame.ProgramCounter))
+		// stack = append(stack, fmt.Sprintf("%s.%s %s:%d (0x%x)", frame.Package, frame.Name, frame.File, frame.LineNumber, frame.ProgramCounter))
+		stack = append(stack, fmt.Sprintf("%s.%s %s:%d", frame.Package, frame.Name, frame.File, frame.LineNumber))
 	}
 	return stack
 }
