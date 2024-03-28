0d7fc2992ba40a94dce21a451b819eb6fbc1c325
Shawn Catanzarite <me@shawncatz.com>
2024-03-28 17:59:41 +0000

    initial

---
+++ errors.go
@@ -0,0 +1,38 @@
+package fae
+
+import (
+	"fmt"
+
+	"github.com/go-errors/errors"
+)
+
+func New(message interface{}) error {
+	return errors.New(message)
+}
+
+func Errorf(format string, args ...interface{}) error {
+	return errors.Errorf(format, args...)
+}
+
+func Wrap(err error, message string) error {
+	return errors.WrapPrefix(err, message, 1)
+}
+
+func Wrapf(err error, format string, args ...interface{}) error {
+	return errors.WrapPrefix(err, fmt.Sprintf(format, args...), 1)
+}
+
+func StackTrace(err error) []string {
+	list := errors.Wrap(err, 1).StackFrames()
+	stack := []string{err.Error()}
+
+	for _, frame := range list {
+		line, _ := frame.SourceLine()
+		stack = append(stack, fmt.Sprintf("%s %s:%d (0x%x)", line, frame.File, frame.LineNumber, frame.ProgramCounter))
+	}
+	return stack
+}
+
+func ErrorStack(err error) string {
+	return string(errors.Wrap(err, 1).Stack())
+}
---
+++ errors_test.go
@@ -0,0 +1,62 @@
+package fae
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+	err := New("test")
+	assert.Error(t, err)
+	assert.Equal(t, "test", err.Error())
+}
+
+func TestErrorf(t *testing.T) {
+	err := Errorf("test %s", "error")
+	assert.Error(t, err)
+	assert.Equal(t, "test error", err.Error())
+}
+
+func TestWrap(t *testing.T) {
+	err := New("test")
+	err = Wrap(err, "wrapped")
+	assert.Error(t, err)
+	assert.Equal(t, "wrapped: test", err.Error())
+}
+
+func TestWrapf(t *testing.T) {
+	err := New("test")
+	err = Wrapf(err, "wrapped %s", "error")
+	assert.Error(t, err)
+	assert.Equal(t, "wrapped error: test", err.Error())
+}
+
+func TestStackTrace(t *testing.T) {
+	err := New("test")
+	stack := StackTrace(err)
+	assert.NotEmpty(t, stack)
+}
+
+func openError() error {
+	_, err := os.ReadFile("fileDoesNotExist")
+	return Wrap(err, "opening file")
+}
+
+func TestStackTrace2(t *testing.T) {
+	err := openError()
+	stack := StackTrace(Wrap(err, "test"))
+	assert.NotEmpty(t, stack)
+	for _, f := range stack {
+		fmt.Printf("  %s\n", f)
+	}
+}
+
+func TestErrorStack(t *testing.T) {
+	err := openError()
+	stack := ErrorStack(Wrap(err, "test"))
+	assert.NotEmpty(t, stack)
+	fmt.Println(stack)
+}
